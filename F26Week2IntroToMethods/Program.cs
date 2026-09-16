daab4e5a1160cd767e92ed5c9e653c19374491ed
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-16 14:37:20 -0400

    value and ref parameters

--- F26Week2IntroToMethods/Program.cs
+++ F26Week2IntroToMethods/Program.cs
@@ -8,25 +8,29 @@
             string s = "hello";
             Circle c = new Circle();
             c.radius = 5;
+            int[] a = { 5, 6, 7 };
 
             Console.WriteLine("Before changes:");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("c.radius = " + c.radius);
+            Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValues(n, s, c);
+            ChangeValues(n, s, c, a);
 
             Console.WriteLine("\nAfter changes:");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("c.radius = " + c.radius);
+            Console.WriteLine("a[0] = " + a[0]);
         }
 
-        static void ChangeValues(int num, string str, Circle cir)
+        static void ChangeValues(int num, string str, Circle cir, int[] arr)
         {
             num = 100;
             str = "bye";
             cir.radius = 100;
+            arr[0] = 100;
         }
     }
 
