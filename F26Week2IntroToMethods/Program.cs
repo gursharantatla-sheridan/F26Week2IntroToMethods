e98e51cba5d1218249a6bc0d6b12b87705b64693
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-16 14:24:04 -0400

    value and ref vars

--- F26Week2IntroToMethods/Program.cs
+++ F26Week2IntroToMethods/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 5;
+            string s = "hello";
+            Circle c = new Circle();
+            c.radius = 5;
+
+            Console.WriteLine("Before changes:");
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("c.radius = " + c.radius);
+
+            ChangeValues(n, s, c);
+
+            Console.WriteLine("\nAfter changes:");
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("c.radius = " + c.radius);
         }
+
+        static void ChangeValues(int num, string str, Circle cir)
+        {
+            num = 100;
+            str = "bye";
+            cir.radius = 100;
+        }
+    }
+
+    class Circle
+    {
+        public int radius;
     }
 }
